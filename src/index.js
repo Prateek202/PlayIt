52eee5b8037777db137a15a3f7e1a8a91e534923
Prateek Thakur <prateek.2022ca069@mnnit.ac.in>
2024-04-21 14:35:23 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import 'dotenv/config'
+// import mongoose from "mongoose";
+// import { DB_NAME} from "./constants";   
+import connectDB from "./db/index.js";  
+
+//Approach 1 to connect DB
+/*
+import express from "express"
+
+
+const app = express()
+;(async() => {
+
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) //await is mandatory        
+        app.on("error",()=>{
+            console.log("ERROR :", error)
+            throw error
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listning on PORT ${process.env.PORT}`);
+        })
+    }
+    catch(error){
+        console.error("ERROR",error);
+        throw err
+    }
+}) ()  
+*/
+
+//Approach 2 
+
+ /*in this approaach we create a saperate file and create all function 
+on that file and import that file in index.js*/
+
+
+connectDB()  
